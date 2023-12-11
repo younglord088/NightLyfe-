1ff21afee9e74156a82fccdd1233c85a052da706
Yash Panjwani <yashmpanjwani.com>
2023-12-11 21:32:26 +0530

    Similar Products Style change

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -37,51 +37,52 @@ const ProductDetails = () => {
 
   return (
     <Layout>
-      <h1 style={{ textAlign: "center", color: "#E7473C", marginBottom: "20px" ,fontSize:"50px"}}>Product Details</h1>
-      <div className="row mt-2">
+      <h1 style={{ textAlign: "center", color: "#E7473C", marginBottom: "20px", fontSize: "50px" }}>Product Details</h1>
+      <div className="row mt-2" style={{ backgroundColor: "#E9F1FA"}}>
         <div className="col-md-6">
           <img
             src={`${process.env.REACT_APP_API}/api/v1/product/products-photo/${product._id}`}
             className="card-img-top"
             alt={product.name}
-            style={{  maxWidth: "100%", maxHeight: "400px", borderRadius: "8px" }}
+            style={{ maxWidth: "100%", maxHeight: "400px", borderRadius: "8px" }}
           />
         </div>
         <div className="col-md-6">
-          <h2 style={{ color: "black" ,fontSize:"40px" }}>Name:- {product.name} </h2>
-          <p style={{ fontSize: "20px" ,textAlign:"left"}}>Description:- {product.description} </p>
+          <h2 style={{ color: "black", fontSize: "40px" }}>Name:- {product.name} </h2>
+          <p style={{ fontSize: "20px", textAlign: "left" }}>Description:- {product.description} </p>
           <h3>Price:- Rs {product.price} </h3>
-          
-          <h4 style={{ fontSize: "30px"}} >Category:- {product.category?.name} </h4>
+          <h4 style={{ fontSize: "30px" }}>Category:- {product.category?.name} </h4>
           <div className="text-md-center">
-          <button className="btn btn-secondary" style={{ marginTop: "10px", backgroundColor: "#E7473C", border: "none" }}>ADD TO CART</button>
-        </div>
+            <button className="btn btn-secondary" style={{ marginTop: "10px", backgroundColor: "#E7473C", border: "none" }}>ADD TO CART</button>
+          </div>
         </div>
       </div>
       <hr />
-      <div className="row container">
-        <h1 style={{ textAlign: "center", color: "#E7473C", marginBottom: "20px" }}>Similar Products</h1>
-        {relatedProducts?.length === 0 && (
-          <h1 className="text-center">No Similar Products Found</h1>
-        )}
-        <div className="d-flex flex-wrap">
-          {relatedProducts?.map((p) => (
-            <div className="card m-2" style={{ width: "18rem" }}>
-              <img
-                src={`${process.env.REACT_APP_API}/api/v1/product/products-photo/${p._id}`}
-                className="card-img-top"
-                alt={p.name}
-                style={{ borderRadius: "8px" }}
-              />
-              <div className="card-body">
-                <h5 className="card-title" style={{ color: "#FF3366" }}>{p.name}</h5>
-                <p className="card-text">{p.description.substring(0, 30)}...</p>
-                <p className="card-text"> Rs {p.price}</p>
-
-                <button className="btn btn-secondary" style={{ backgroundColor: "#FF3366", border: "none" }}>ADD TO CART</button>
+      <div className="container" style={{ width: "150%" }}>
+        <h1 style={{ textAlign: "center", color: "#E7473C", marginBottom: "20px" }}>Similar Recommendations:-</h1>
+        <div className="row" style={{ backgroundColor: "#E9F1FA"}}>
+          {relatedProducts?.length === 0 ? (
+            <h1 className="text-center">No Similar Products Found</h1>
+          ) : (
+            relatedProducts?.map((p) => (
+              <div className="col-md-6 mb-3" key={p._id}>
+                <div className="card" style={{ width: "200%", marginLeft:200 }}>
+                  <img
+                    src={`${process.env.REACT_APP_API}/api/v1/product/products-photo/${p._id}`}
+                    className="card-img-top"
+                    alt={p.name}
+                    style={{ borderRadius: "8px" }}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title" style={{ color: "#FF3366" }}>{p.name}</h5>
+                    <p className="card-text">{p.description.substring(0, 30)}...</p>
+                    <p className="card-text"> Rs {p.price}</p>
+                    <button className="btn btn-secondary" style={{ backgroundColor: "#FF3366", border: "none" }}>ADD TO CART</button>
+                  </div>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
       </div>
     </Layout>
